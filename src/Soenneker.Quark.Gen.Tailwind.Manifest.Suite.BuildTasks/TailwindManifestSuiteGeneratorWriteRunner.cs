9caadced6597345f2b569ffd1c9fcd74cdd8dd88
soenneker <jake@soenneker.com>
2026-09-03 22:01:39 -0500

    Add interface inheritdoc references

--- src/Soenneker.Quark.Gen.Tailwind.Manifest.Suite.BuildTasks/TailwindManifestSuiteGeneratorWriteRunner.cs
+++ src/Soenneker.Quark.Gen.Tailwind.Manifest.Suite.BuildTasks/TailwindManifestSuiteGeneratorWriteRunner.cs
@@ -17,6 +17,7 @@ using System.Threading.Tasks;
 
 namespace Soenneker.Quark.Gen.Tailwind.Manifest.Suite.BuildTasks;
 
+/// <inheritdoc cref="ITailwindManifestSuiteGeneratorWriteRunner" />
 public sealed partial class TailwindManifestSuiteGeneratorWriteRunner : ITailwindManifestSuiteGeneratorWriteRunner
 {
     private const string _suiteManifestFileName = "quark-suite-tailwind-manifest.txt";
