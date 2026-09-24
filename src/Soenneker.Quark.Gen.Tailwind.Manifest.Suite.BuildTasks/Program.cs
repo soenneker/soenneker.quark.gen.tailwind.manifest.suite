70b596de2c54f06ca247ec798b87acd3518e137a
soenneker <jake@soenneker.com>
2026-09-23 21:18:30 -0500

    Improve external generator startup and incremental builds

--- src/Soenneker.Quark.Gen.Tailwind.Manifest.Suite.BuildTasks/Program.cs
+++ src/Soenneker.Quark.Gen.Tailwind.Manifest.Suite.BuildTasks/Program.cs
@@ -1,3 +1,4 @@
+using Soenneker.Quark.Gen.Tailwind.Manifest.Suite.BuildTasks.Abstract;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -26,12 +27,22 @@ public sealed class Program
 
         try
         {
-            await CreateHostBuilder(args).RunConsoleAsync(_cts.Token);
+            var services = new ServiceCollection();
+            services.AddLogging(logging => logging.AddConsole());
+            Startup.ConfigureServices(services);
+
+            await using ServiceProvider provider = services.BuildServiceProvider();
+            await using AsyncServiceScope scope = provider.CreateAsyncScope();
+            Environment.ExitCode = await scope.ServiceProvider.GetRequiredService<ITailwindManifestSuiteGeneratorWriteRunner>().Run(args, _cts.Token);
+        }
+        catch (OperationCanceledException) when (_cts.IsCancellationRequested)
+        {
+            Environment.ExitCode = 130;
         }
         catch (Exception e)
         {
             await Console.Error.WriteLineAsync($"Stopped program because of exception: {e}");
-            throw;
+            Environment.ExitCode = 1;
         }
         finally
         {
